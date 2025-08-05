11c04f03269ade3fb6f413914ec343fedfecf124
hackheroin28 <165154532+hackheroin28@users.noreply.github.com>
2025-08-05 11:26:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -493,4 +493,4 @@ def contact():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=3450)
+    app.run(debug=True,host='0.0.0.0', port=8000)
